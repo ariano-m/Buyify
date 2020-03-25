f005f024820b65d961d89ac98d3aed72224d17f6
J Portal <j.portal@outlook.com>
2020-03-25 19:44:04 +0100

    Send email on sign up

--- src/main/java/com/buyify/user/UserController.java
+++ src/main/java/com/buyify/user/UserController.java
@@ -1,19 +1,15 @@
 package com.buyify.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.security.Principal;
-import java.util.List;
 
 @Controller
 public class UserController {
@@ -36,10 +32,12 @@ public class UserController {
     @PostMapping("/nuevoUsuario")
     public String registrarse(Model model, @RequestParam String name, @RequestParam String username,
                               @RequestParam String password, @RequestParam String email) {
+        User user = userRepository.save(new User(name, username, email, password, "USER"));
 
-        User user = new User(name, username, email, password);
-        userRepository.save(user);
         model.addAttribute("username", user.getUsername());
+
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.exchange("http://localhost:9999/mail/" + user.getId(), HttpMethod.GET, null, Void.class);
 
         return "registered";
     }
