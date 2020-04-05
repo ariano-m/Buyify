0e88d29063b4ee8691641ea1c4a40da010bbd2b5
J Portal <j.portal@outlook.com>
2020-04-05 13:50:02 +0200

    Enable redis session

--- src/main/java/com/buyify/BuyifyApplication.java
+++ src/main/java/com/buyify/BuyifyApplication.java
@@ -3,10 +3,9 @@ package com.buyify;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+@EnableRedisHttpSession
 @EnableCaching
 @SpringBootApplication
 public class BuyifyApplication {
@@ -14,16 +13,4 @@ public class BuyifyApplication {
     public static void main(String[] args) {
         SpringApplication.run(BuyifyApplication.class, args);
     }
-
-//    @Bean
-//    JedisConnectionFactory jedisConnectionFactory() {
-//        return new JedisConnectionFactory();
-//    }
-//
-//    @Bean
-//    public RedisTemplate<String, Object> redisTemplate() {
-//        RedisTemplate<String, Object> template = new RedisTemplate<>();
-//        template.setConnectionFactory(jedisConnectionFactory());
-//        return template;
-//    }
 }
